c6d830bbbbec5738e781f99f934faa78717cc018
Siddharth Gupta <20bcs215@iiitdmj.ac.in>
2021-10-09 21:38:59 +0530

    Razorpay integrated

--- src/components/MovieHero/MovieInfo.Component.jsx
+++ src/components/MovieHero/MovieInfo.Component.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import PaymentModal from "../PaymentModal/Payment.Component";
 
 const MovieInfo = ({ movie, genres }) => {
+    const [price, setPrice] = useState(0);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const rentMovie = () => {
+        setPrice(149.99);
+        setIsOpen(true);
+    }
+    const buyMovie = () => {
+        setPrice(499.99);
+        setIsOpen(true);
+    }
+
     const time = movie.runtime;
     return (
         <>
+            <PaymentModal isOpen={isOpen} setIsOpen={setIsOpen} price={price} />
             <div className="flex flex-col gap-8">
                 <h1 className="text-white text-5xl font-bold">
                     {movie.movie_title}
@@ -14,10 +28,10 @@ const MovieInfo = ({ movie, genres }) => {
                     <h4>{parseInt(time / 60)}h {time % 60}m  •  {genres}  •  {movie.release_date} </h4>
                 </div>
                 <div className="flex items-center gap-3 w-full">
-                    <button className="bg-red-500 w-full py-3 text-white font-semibold rounded-lg">
+                    <button onClick={rentMovie} className="bg-red-500 w-full py-3 text-white font-semibold rounded-lg">
                         Rent ₹ 149.99
                     </button>
-                    <button className="bg-red-500 w-full py-3 text-white font-semibold rounded-lg">
+                    <button onClick={buyMovie} className="bg-red-500 w-full py-3 text-white font-semibold rounded-lg">
                         Buy ₹ 499.99
                     </button>
                 </div>
